c9696eb01a527d690505d13d150adc1fa4a58d5d
Vans-png-sys <befanalfiansyach0908@gmail.com>
2025-12-23 11:45:49 +0700

    Add files via upload

--- src/Componenets/Mapel/Kamis.jsx
+++ src/Componenets/Mapel/Kamis.jsx
@@ -1,11 +1,11 @@
-const Jumat = () => {
+const Kamis = () => {
 	return (
 		<>
 			<div
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">TJKT Server-Cisco</div>
+				<div className="w-[50%] text-base  font-medium">TJKT Cloud-NAS</div>
 				<div className="flex justify-center items-center text-sm">07.00-09.40</div>
 			</div>
 			<div
@@ -19,25 +19,33 @@ const Jumat = () => {
 				className="border-t-2 border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
 				data-aos-duration="700">
-				<div className="w-[50%] text-base  font-medium">Mikrotik</div>
+				<div className="w-[50%] text-base  font-medium">PKK</div>
 				<div className="flex justify-center items-center text-sm">10.00-11.20</div>
 			</div>
 			<div
-				className="border-b-2 flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
-				data-aos-duration="600">
+				data-aos-duration="800">
 				<div className="w-[50%] text-base  font-medium">Istirahat</div>
-				<div className="flex justify-center items-center text-sm">11.20-13.00</div>
+				<div className="flex justify-center items-center text-sm">11.20-12.20</div>
 			</div>
 			<div
-				className=" flex justify-between py-[0.50rem] w-72 px-3 opacity-60"
+				className=" border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
 				data-aos="fade-up"
-				data-aos-duration="600">
-				<div className="w-[50%] text-base  font-medium">TJKT Jaringan Lanjutan</div>
-				<div className="flex justify-center items-center text-sm">13.00-14.20</div>
+				data-aos-duration="800">
+				<div className="w-[50%] text-base  font-medium">TJKT Linux</div>
+				<div className="flex justify-center items-center text-sm">12.20-13.40</div>
+			</div>
+
+			<div
+				className="border-b-2 border-white flex justify-between py-[0.50rem] w-72 px-3"
+				data-aos="fade-up"
+				data-aos-duration="1000">
+				<div className="w-[50%] text-base font-medium">B.Inggris</div>
+				<div className="flex justify-center items-center text-sm">13.40-15.00</div>
 			</div>
 		</>
 	)
 }
 
-export default Jumat
+export default Kamis
